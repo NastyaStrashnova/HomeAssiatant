3f90c2a514fb28f305bb0ba5e1821070a644f078
Anastasiia <nastya.strashnova@gmail.com>
2025-06-19 18:13:48 +0200

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,7 +1,1 @@
-from faster_whisper import WhisperModel
-from sentence_transformers import SentenceTransformer
-
-print("Loading models...")
-whisper_model = WhisperModel("base", compute_type="int8")  # Or "tiny"
-intent_model = SentenceTransformer("all-MiniLM-L6-v2")
-print("Models loaded.")
+# Load your model
